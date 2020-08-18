746e9602fc85c737c988eab542561bf2422d6a75
Mantej Singh Lamba <mantejkhalsa@gmail.com>
2020-08-17 17:55:43 -0700

    Update

--- image.py
+++ image.py
@@ -3,10 +3,14 @@ import glob, os
 
 size = 300, 300
 
-for infile in glob.glob("./input/*.jpg"):
+for infile in glob.glob("./input/*.jpg" or "./input/*.png"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
     im.thumbnail(size)
-    outfile = file.replace("input", "output")
-    outfile = outfile + "_t.jpg"
-    im.save(outfile, "JPEG")
+    file.replace("input", "output")
+    if ext == ".jpg":
+        outfile = outfile + "_t.jpg"
+        im.save(outfile, "JPEG")
+    elif ext == ".png":
+        outfile = outfile +"_t.png"
+        im.save(outfile, "PNG")
