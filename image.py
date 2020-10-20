51b8bd0f354a3c322896514dd04b9d06622be69e
Prabhleen Kaur Lamba <khalsaprabhleen@gmail.com>
2020-10-19 17:26:25 -0700

    few updates

--- image.py
+++ image.py
@@ -3,10 +3,16 @@ import glob, os
 
 size = 300, 300
 
-for infile in glob.glob("./input/*.jpg"):
+
+for infile in glob.glob("images/*.*"):
     file, ext = os.path.splitext(infile)
+    if ext == ".pdf":
+        continue
     im = Image.open(infile)
     im.thumbnail(size)
-    outfile = file.replace("input", "output")
-    outfile = outfile + "_t.jpg"
-    im.save(outfile, "JPEG")
+    outfile = file.replace("images", "output")
+    outfile = outfile + "_t"+ext
+    if ext == ".jpg":
+        im.save(outfile, "JPEG")
+    if ext == ".png":
+        im.save(outfile, "PNG")
